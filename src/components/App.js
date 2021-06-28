5aa5735095dd9b4de546098df222bf06fb5a792e
Salome Ranson <salomeranson@gmail.com>
2021-06-28 12:02:57 -0400

    made successful api call to unsplash api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,12 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
    
    onSearchSubmit(term) {
-       console.log(term);
+       axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term }, 
+            headers: {
+                    Authorization: 'Client-ID Jka735JO2x_3jcuOjhmaUzQT9CKGuTgoe4_rXW59OwQ'
+            } 
+       });
    }
 
     render() { 
