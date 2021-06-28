e47babf0ce9d47d01961bc2599c0c1b9a0be841b
Salome Ranson <salomeranson@gmail.com>
2021-06-28 11:19:47 -0400

    added form submit method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: ''};
+
+    // It is important to use the Arrow Function here so that the 'this' keyword points to the SearchBar class - will get 'undefined otherwise'
+    onFormSubmit = (event) => {
+        event.preventDefault();             // prevents page from refreshing
+        console.log(this.state.term);
+    };
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                            <input 
+                                type="text" 
+                                placeholder="Search for a term"
+                                value={this.state.term} 
+                                onChange={ e => this.setState({ term: e.target.value })} 
+                            />
                     </div>
                 </form>
             </div>
