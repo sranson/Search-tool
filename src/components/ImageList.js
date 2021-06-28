9fa3e89a05e80c1b0b60ed14307b0795f1b9f4ef
Salome Ranson <salomeranson@gmail.com>
2021-06-28 12:51:46 -0400

    rendering photos of search term via ImageList component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 
-const ImageList = (props) => {
+const ImageList = props => {
     console.log(props);
-    return (
-        <div>Imagelist</div>
-    )
+    const images = props.images.map((image) => {
+        return <img key={image.id} src={image.urls.regular} alt={image.alt_description}  />
+    });
+
+    return <div>{images}</div>
 }
 
 export default ImageList;
