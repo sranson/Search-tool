1e6dbe3750777b1b5a4466f923678bcaee96e07e
Salome Ranson <salomeranson@gmail.com>
2021-06-28 14:01:46 -0400

    successfully getting the clientHeight for each image from the VDOM ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,11 +3,24 @@ import './ImageList.css';
 
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         const {alt_description, urls} = this.props.image;
         return(
             <div>
-                <img alt={alt_description} src={urls.regular} />
+                <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
             </div>
         )
     }
