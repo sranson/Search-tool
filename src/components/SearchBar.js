46a628381ee3492dc6b2fe5ea9393a52885b5d4d
Salome Ranson <salomeranson@gmail.com>
2021-06-28 10:00:38 -0400

    added file structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input />
+                </form>
+            </div>
+        );
+    };
+};
+
+
+export default SearchBar;
