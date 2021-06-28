8405d2ca97e36a6e7faa8ae439a46dbdda43ee38
Salome Ranson <salomeranson@gmail.com>
2021-06-28 12:58:13 -0400

    refactored code by destructuring images properties

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 
 const ImageList = props => {
-    console.log(props);
-    const images = props.images.map((image) => {
-        return <img key={image.id} src={image.urls.regular} alt={image.alt_description}  />
+    // console.log(props);
+    const images = props.images.map(({id, urls, alt_description }) => {
+        return <img key={id} src={urls.regular} alt={alt_description}  />
     });
 
     return <div>{images}</div>
